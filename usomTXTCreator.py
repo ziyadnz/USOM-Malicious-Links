57568bb497b69ce149bcef136d32a7ff086d6ca2
Ziya Deniz <dziyaaaa@gmail.com>
2024-06-08 01:45:00 +0300

    Update usomTXTCreator.py

--- usomTXTCreator.py
+++ usomTXTCreator.py
@@ -1,6 +1,5 @@
 import requests
 import re
-from urllib.parse import urlparse
 
 # URL of the text file
 url = 'https://www.usom.gov.tr/url-list.txt'
@@ -11,44 +10,40 @@ def fetch_url_content(url):
     response.raise_for_status()
     return response.text
 
-# Function to extract domain or IP from URL
-def extract_host(url):
-    parsed_url = urlparse(url)
-    return parsed_url.netloc
-
 # Function to separate IP addresses and domain names
 def separate_ip_and_domain(lines):
-    ip_pattern = re.compile(r'^(\d{1,3}\.){3}\d{1,3}$')
-    ip_addresses = set()  # Use a set to avoid duplicates
-    domains = set()  # Use a set to avoid duplicates
+    ip_pattern = re.compile(r'(\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b)')
+    ip_addresses = []
+    domains = []
 
     for line in lines:
         line = line.strip()
-        host = extract_host(line)
-        if ip_pattern.match(host):
-            ip_addresses.add(host)
+        if ip_pattern.match(line):
+            ip_addresses.append(line)
         else:
-            domains.add(host)
+            domains.append(line)
 
-    return list(ip_addresses), list(domains)
+    return ip_addresses, domains
 
 # Function to write to files
 def write_to_files(ip_addresses, domains):
     with open('ipv4.txt', 'w') as ip_file:
         for ip in ip_addresses:
-            ip_file.write(ip + '\n')
+            ip_file.write(ip.split('/')[0] + '\n')
 
     with open('domain.txt', 'w') as domain_file:
         for domain in domains:
-            domain_file.write(domain + '\n')
+            domain_file.write(domain.split('/')[0] + '\n')
 
 # Main function
 def main():
     content = fetch_url_content(url)
     lines = content.splitlines()
+
+
     
-    # Get the latest 100 lines
-    latest_lines = lines[-100:]
+    # Get the latest 500 lines
+    latest_lines = lines[-500:]
     
     # Separate IPs and Domains
     ip_addresses, domains = separate_ip_and_domain(latest_lines)
